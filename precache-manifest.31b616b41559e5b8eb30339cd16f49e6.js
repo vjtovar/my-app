a42fe500b55ce64921b78d60825ae52c686fc1d7
vjtovar <vjtovar25@gmail.com>
2022-06-21 19:58:08 -0700

    Updates

---
+++ precache-manifest.31b616b41559e5b8eb30339cd16f49e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "558dfa310834bd53fa45",
+    "url": "/my-app/static/css/main.a60712e2.chunk.css"
+  },
+  {
+    "revision": "558dfa310834bd53fa45",
+    "url": "/my-app/static/js/main.558dfa31.chunk.js"
+  },
+  {
+    "revision": "fc50a9dc01636443123b",
+    "url": "/my-app/static/js/1.fc50a9dc.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "59bfe95c5e4dc883171f8352cf40e521",
+    "url": "/my-app/static/media/cv.59bfe95c.pdf"
+  },
+  {
+    "revision": "92c16e484bf5321b0119a5515cb9b64c",
+    "url": "/my-app/static/media/me.92c16e48.png"
+  },
+  {
+    "revision": "9542863600dfe2f38e070a21c1a085e3",
+    "url": "/my-app/static/media/me-about.95428636.jpg"
+  },
+  {
+    "revision": "4b515d6d205a91fbc702e4e516afa3fd",
+    "url": "/my-app/static/media/portfolio1.4b515d6d.jpg"
+  },
+  {
+    "revision": "cc2c392523a6e267cf2e8e672a31ba75",
+    "url": "/my-app/static/media/portfolio2.cc2c3925.jpg"
+  },
+  {
+    "revision": "ef086065072234382dc61983151856f2",
+    "url": "/my-app/static/media/bg-texture.ef086065.png"
+  },
+  {
+    "revision": "25e5c3520cf9d79c96b3852aae6ec813",
+    "url": "/my-app/index.html"
+  }
+];
