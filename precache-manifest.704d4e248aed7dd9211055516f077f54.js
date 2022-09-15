e6362ff7cf851d2bf5423acd8ff2ba40fd94f950
Valerie Tovar <vjtovar@Valeries-MacBook-Air.local>
2022-09-14 18:50:25 -0700

    Updates

---
+++ precache-manifest.704d4e248aed7dd9211055516f077f54.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e1d9de51387462784ea",
+    "url": "/my-app/static/css/main.153a1b42.chunk.css"
+  },
+  {
+    "revision": "7e1d9de51387462784ea",
+    "url": "/my-app/static/js/main.7e1d9de5.chunk.js"
+  },
+  {
+    "revision": "3d502f9a1251430603c4",
+    "url": "/my-app/static/js/1.3d502f9a.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "f132d591c898f774a0b71420107c5776",
+    "url": "/my-app/static/media/cv.f132d591.pdf"
+  },
+  {
+    "revision": "92c16e484bf5321b0119a5515cb9b64c",
+    "url": "/my-app/static/media/me.92c16e48.png"
+  },
+  {
+    "revision": "9542863600dfe2f38e070a21c1a085e3",
+    "url": "/my-app/static/media/me-about.95428636.jpg"
+  },
+  {
+    "revision": "4b515d6d205a91fbc702e4e516afa3fd",
+    "url": "/my-app/static/media/portfolio1.4b515d6d.jpg"
+  },
+  {
+    "revision": "cc2c392523a6e267cf2e8e672a31ba75",
+    "url": "/my-app/static/media/portfolio2.cc2c3925.jpg"
+  },
+  {
+    "revision": "ef086065072234382dc61983151856f2",
+    "url": "/my-app/static/media/bg-texture.ef086065.png"
+  },
+  {
+    "revision": "8a6d5762c3972fbf3fd2409c8bad53c9",
+    "url": "/my-app/index.html"
+  }
+];
